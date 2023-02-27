50a04f0bf4bf9c7faec1af6582133fb1b9440147
MusaddekAli <musaddekali.dev@gmail.com>
2023-02-27 15:14:55 +0600

    dummy_data_updated

--- src/modules/VideoGallery/useHome.ts
+++ src/modules/VideoGallery/useHome.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { VIDEOS } from "./dummy_video_data";
 
 const useHome = () => {
   const [isVideoSidebarOpen, setIsVideoSidebarOpen] = useState(false);
   const [isVideoSearchOpen, setIsVideoSearchOpen] = useState(false);
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState(VIDEOS);
 
   const toggleVideoSidebar = () => {
     setIsVideoSidebarOpen(!isVideoSidebarOpen);
@@ -14,18 +15,18 @@ const useHome = () => {
     setIsVideoSearchOpen(!isVideoSearchOpen);
   };
 
-  async function getVideosData() {
-    try {
-      const res = await axios.get("api/videos");
-      setVideos(res.data);
-    } catch (error) {
-      console.log("data error -> ", error);
-    }
-  }
+  // async function getVideosData() {
+  //   try {
+  //     const res = await axios.get("api/videos");
+  //     setVideos(res.data);
+  //   } catch (error) {
+  //     console.log("data error -> ", error);
+  //   }
+  // }
 
-  useEffect(() => {
-    getVideosData();
-  }, []);
+  // useEffect(() => {
+  //   getVideosData();
+  // }, []);
 
   return {
     isVideoSidebarOpen,
